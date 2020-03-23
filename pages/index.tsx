402c6d5239b3a6069fe3c62d12bf4c088c5caaa9
Joshua Mora <jshm101@gmail.com>
2020-03-22 18:07:47 -0700

    fix: proper typescript config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
-const Index = () => (
-  <h2>Hello World</h2>
-)
+const Index = () => {
+  return (
+    <div>
+      <h2>Hello World</h2>
+    </div>
+  )
+}
 
 export default Index
